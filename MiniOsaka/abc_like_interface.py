5aa1508adf80d440dc3083a19b6844410c2239b7
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2018-05-18 18:18:42 +0900

    update

--- MiniOsaka/abc_like_interface.py
+++ MiniOsaka/abc_like_interface.py
@@ -4,11 +4,15 @@ class InterfaceGreet(metaclass=ABCMeta):
     """
     Interface like in Python
     """
+    @property
+    @abstractmethod
+    def name(self): ...
+
     @abstractmethod
     def greet(self, other): ...
 
 class Person(InterfaceGreet):
-    pass
+    name = ''
 
 p = Person() # error: Cannot instantiate abstract class
              # 'Person' with abstract attribute 'greet'
